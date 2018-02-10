0626b75aa050720f8cf0c9b5770a6585e61a727b
YatchiYa <35928424+YatchiYa@users.noreply.github.com>
2018-02-10 16:21:29 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
-// grow script
+
+// *****************           VueJs                     **********************************
+
+
+// input list  in general list : using vue.js
+Vue.component('general-task-list', {
+
+	template:'<div> <slot></slot> </div>'
+
+});
+
+
+
+
+new Vue ({
+
+	el: '#task',
+	data: {
+		newtask: '',
+		tasks: []
+		},
+
+	methods: {
+		addTask() {
+
+			this.tasks.push(this.newtask);
+			this.newtask='';		
+		}
+	},
+});
+
+
+
+ // the different boxes : 
+
+
+Vue.component('important_urgent-list','important-list','urgent-list','not_important_urgent-list', {
+
+	template:'<div> <slot></slot> </div>'
+
+});
+
+
+
+
+// ***************************    Jquery  ******************************
+
+
+

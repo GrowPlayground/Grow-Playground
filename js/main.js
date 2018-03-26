80968707b25f7a7f86dd203333cbf07d8f9a5d73
utopie98 <36268199+utopie98@users.noreply.github.com>
2018-03-26 14:30:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -307,10 +307,6 @@ $(document).ready( function()
 
 			// calendar 
 
-function setStyle(id,style,value)
-{
-    id.style[style] = value;
-}
 function opacite(el,opacity)
 {
     setStyle(el,"filter:","alpha(opacity="+opacity+")");
@@ -324,67 +320,98 @@ function calendrier()
     var jour = date.getDate();
     var moi = date.getMonth();
     var annee = date.getYear();
+    var jour_calendrier=0;
     if(annee<=200)
     {
         annee += 1900;
     }
-    mois = new Array('Janvier', 'F&eacute;vrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Ao&ucirc;t', 'Septembre', 'Octobre', 'Novembre', 'D&eacute;cembre');
+    mois = new Array('Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre');
     jours_dans_moi = new Array(31,28,31,30,31,30,31,31,30,31,30,31);
     if(annee%4 == 0 && annee!=1900)
     {
         jours_dans_moi[1]=29;
     }
-    total = jours_dans_moi[moi];
+    var total = jours_dans_moi[moi];
     var date_aujourdui = jour+' '+mois[moi]+' '+annee;
-    dep_j = date;
-    dep_j.setDate(1);
-    if(dep_j.getDate()==2)
-    {
-        dep_j=setDate(0);
-    }
-    dep_j = dep_j.getDay();
-    document.write('<table class="cal_calendrier" onload="opacite(document.getElementById(\'cal_body\'),20);"><tbody id="cal_body"><tr><th colspan="7">'+date_aujourdui+'</th></tr>');
+    var citations = ["The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees The Opportunity In Every Difficulty .- Winston Churchill", "You learn more from failure than from success. Don't let it stop you. Failure builds character", "“If you have a dream, don’t just sit there. Gather courage to believe that you can succeed and leave no stone unturned to make it a reality.” Dr Roopleen","The world makes way for the man who knows where he is going. - Ralph Emerson","Winning starts with beginning.” Anonymous","What you do makes a difference, and you have to decide what kind of difference you want to make.” - Jane Goodall","Once we accept our limits, we go beyond them. - Albert Einstein ","If you can dream it, you can do it. - Walt Disney","Life isn't about finding yourself. Life is about creating yourself. - Bernard Shaw","“It's not who you think you are that holds you back it's who you think you're not.”","Every dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.","“The first follower is an underestimated form of leadership. The first follower is what transforms a lone nut into a leader.” ― Derek Sivers"]; 
+    
+    document.write('<table class="cal_calendrier" onload="opacite(document.getElementById(\'cal_body\'),20);"><tbody id="cal_body"><tr><th colspan="7">'+mois[moi]+'</th></tr>');
    
-    sem = 0;
-    for(i=1;i<=dep_j-4;i++)
-    {
-        document.write('<td class="cal_jours_av_ap">'+(jours_dans_moi[moi-1]-dep_j+i)+'</td>');
-        sem++;
+    document.write('<tr>');
+    for(i=0;i<=4;i++)
+    {   
+        jour_calendrier=jour-7+i;
+        document.write('<td>'+jour_calendrier+'</td>');
+        
     }
-    for(i=1;i<=total-3;i++)
+  
+    document.write('</tr>');
+    document.write('<tr>');
+     for(i=0;i<=4;i++)
     {
-        if(sem==0)
-        {
-            document.write('<tr>');
-        }
-        if(jour==i)
-        {
-            document.write('<td>'+i+'</td>');
-        }
-        else
-        {
-            document.write('<td>'+i+'</td>');
-        }
-        sem++;
-        if(sem==(5))
-        {
-            document.write('</tr>');
-            sem=0;
-        }
+        jour_calendrier=jour-2+i;
+        document.write('<td>'+jour_calendrier+'</td>');
+        
     }
-    for(i=1;sem!=0;i++)
+  
+    document.write('</tr>');
+    document.write('<tr>');
+    for(i=0;i<=4;i++)
     {
-        document.write('<td class="cal_jours_av_ap">'+i+'</td>');
-        sem++;
-        if(sem==(5))
-        {
-            document.write('</tr>');
-            sem=0;
-        }
+        jour_calendrier=jour+3+i;
+        document.write('<td>'+jour_calendrier+'</td>');
+        
     }
+ 
+    document.write('</tr>');
+    document.write('<tr>');
+    for(i=0;i<=4;i++)
+    {    
+ 
+        jour_calendrier=jour+8+i;
+        if (jour_calendrier>jours_dans_moi[moi]){
+            jour_calendrier=0;
+
+
+        }
+         document.write('<td>'+jour_calendrier+'</td>');
+        
+    }
+ 
+
+   
+
+    /*
+        $("body").on("click","td", function(){
+            $(".cal_calendrier").append('<div id="notes"><p>Notes</p><textarea  name="Notes" id="Notes" rows="10" cols="20" placeholder="veuillez entrer vos notes"></textarea><menu id="menuNotes"><button id="cancel" type="reset">Annuler</button><button type="submit">Confirmer</button></menu></div>');     
+        
+        });*/
+        $("#notes").hide();
+        $("body").on("click","td", function(){
+            $("#notes").show();
+
+
+
+
+
+        });
+        $("body").on("click","#cancel", function(){
+            $("#notes").hide();
+                });
+
+
+        $("body").on("click",".incr", function(){
+            $("#notess").css({
+                'border-top':'5px solid red'
+            })
+        });
+    
+    document.write('</tr>');
+    document.write('<th colspan=7><marquee behavior="scroll">The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees The Opportunity In Every Difficulty .- Winston Churchill</marquee> </th>');
     document.write('</tbody></table>');
     opacite(document.getElementById('cal_body'),70);
     return true;
+   
 }
 
 
